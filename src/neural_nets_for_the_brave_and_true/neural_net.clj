7029ef83013c246932b064a9b52b3e8fa9c869a2
Jasmine Samra <jsamra@cj.com>
2017-05-12 16:53:46 -0700

    Add feedforward capability and start on backpropagation

--- src/neural_nets_for_the_brave_and_true/neural_net.clj
+++ src/neural_nets_for_the_brave_and_true/neural_net.clj
@@ -4,4 +4,21 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println args))
+
+(def e 2.7182818)
+
+(defn dot-product [v1 v2]
+  (reduce + (map (partial apply *) (map vector v1 v2))))
+
+(defn sigmoid [z]
+  (let [exponent (Math/pow e (- z))]
+    (/ 1 (+ 1 exponent))))
+
+(defn feed-layer [inputs layer]
+  (map (fn [neuron] (sigmoid (dot-product neuron inputs))) layer))
+
+(defn run-net [net inputs]
+  (let [results1 (feed-layer inputs (first net))
+        results2 (feed-layer results1 (second net))]
+    results2))
--- test/neural_nets_for_the_brave_and_true/core_test.clj
+++ test/neural_nets_for_the_brave_and_true/core_test.clj
@@ -2,6 +2,40 @@
   (:require [clojure.test :refer :all]
             [neural-nets-for-the-brave-and-true.neural-net :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def neuron1 [0.5 0.5])
+(def neuron2 [0.5 0.5])
+(def neuron3 [0.5 0.5])
+(def neuron4 [0.5 0.5 0.5])
+
+(def layer1 [neuron1 neuron2 neuron3])
+(def layer2 [neuron4])
+
+(def trained-xor [layer1 layer2])
+
+(defn train-net [net data labels]
+  net)
+
+(deftest train-single-neuron-net
+  (testing
+    (is (= (train-net [[[0.5 0.7]]] [1 0.5] [1])
+           [[[0.4 0.4]]]))))
+
+(deftest dot-product-works
+  (testing "[0.5 1.0] . [0.2 0.3] == 0.4"
+    (is (= (dot-product [0.5 1.0] [0.2 0.3]) 0.4))))
+
+(deftest trained-zero-zero-is-zero
+  (testing "0 ^ 0 == 0"
+    (is (= (run-net trained-xor [0 0]) 0))))
+
+(deftest trained-zero-one-is-one
+  (testing "0 ^ 1 == 0"
+    (is (= (run-net trained-xor [0 1]) 1))))
+
+(deftest trained-one-zero-is-one
+  (testing "1 ^ 0 == 0"
+    (is (= (run-net trained-xor [1 0]) 1))))
+
+(deftest trained-one-one-is-zero
+  (testing "1 ^ 1 == 0"
+    (is (= (run-net trained-xor [1 1]) 0))))
