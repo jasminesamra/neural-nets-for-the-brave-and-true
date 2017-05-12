a4ed6a23f8fa6716597e2f60f6c22825b5474fe6
Jasmine Samra <jsamra@cj.com>
2017-05-11 19:36:09 -0700

    Initial commit - hopefully I did the gitignore right

---
+++ src/neural_nets_for_the_brave_and_true/neural_net.clj
@@ -0,0 +1,7 @@
+(ns neural-nets-for-the-brave-and-true.neural-net
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/neural_nets_for_the_brave_and_true/core_test.clj
@@ -0,0 +1,7 @@
+(ns neural-nets-for-the-brave-and-true.core-test
+  (:require [clojure.test :refer :all]
+            [neural-nets-for-the-brave-and-true.neural-net :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
